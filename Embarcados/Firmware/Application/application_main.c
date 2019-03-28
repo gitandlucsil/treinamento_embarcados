025c09c49de027b15219acdf285c39ff6d8f7a26
andre <andlucsil1221@gmail.com>
2019-03-28 17:52:29 -0300

    criado diretorio Application, seus arquivos e links simbolicos

---
+++ Embarcados/Firmware/Application/application_main.c
@@ -0,0 +1,12 @@
+/*
+ * application_main.c
+ *
+ *  Created on: 28 de mar de 2019
+ *      Author: andre
+ */
+#include "application_main.h"
+void app_run(void)
+{
+	while (1) {
+	}
+}
---
+++ Embarcados/Firmware/Application/application_main.h
@@ -0,0 +1,12 @@
+/*
+ * application_main.h
+ *
+ *  Created on: 28 de mar de 2019
+ *      Author: andre
+ */
+#ifndef APPLICATION_MAIN_H_
+#define APPLICATION_MAIN_H_
+
+void app_run(void);
+
+#endif /* APPLICATION_MAIN_H_ */
